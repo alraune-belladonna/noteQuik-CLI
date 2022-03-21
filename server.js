57e5da3cbd33b0b2e69032d7f4a70a240e2cbd76
Albedo <raincadence27@gmail.com>
2022-03-20 17:53:06 -0700

    update notes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const notes = require('./db/db.json')
 
 const path = require('path')
 
@@ -9,10 +10,19 @@ app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(_dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(_dirname, 'public', 'notes.html'))
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+})
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+app.post('/api/notes', (req, res) => {
+  let newNote = {
+    title: req.body.title,
+    text:req.body.text
+  }
 })
 
 app.listen(3000 || process.env.PORT)
