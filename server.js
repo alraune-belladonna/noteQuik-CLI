31eb7fed4c5e05abd73287926d32865f82a19944
Albedo <raincadence27@gmail.com>
2022-03-22 02:45:48 -0700

    routing fix

--- server.js
+++ server.js
@@ -3,17 +3,14 @@ const app = express()
 let notes = require('./db/db.json')
 const { uid } = require('uid')
 const path = require('path')
+const htmlRoute = require('./public/assets/js/htmlRoute')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
-})
+app.use('/', htmlRoute)
+
 app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
